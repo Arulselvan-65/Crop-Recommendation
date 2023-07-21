352a9c7485ab7e2523d82f6d20c7f863b936e909
Arulselvan-65 <121343334+Arulselvan-65@users.noreply.github.com>
2023-07-21 23:44:23 +0530

    Update Majestic_Agro.py

--- Majestic_Agro.py
+++ Majestic_Agro.py
@@ -3,8 +3,6 @@ import streamlit as st
 from PIL import Image
 import altair as at
 import csv
-from plyer import gps
-from kivy.clock import mainthread
 st.set_page_config(layout="wide")
 hide = """
          <style>
@@ -41,24 +39,5 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-lat1 = " "
-lon1 = " "
-@mainthread
-def on_location (self, **kwargs):
-    global lat1
-    global lon1
-    for k, v in kwargs.items ():
-        if k == "speed":
-            break
-        if k == "lat":
-            lat1 = f"{str (v)}"
-        if k == "lon":
-            lon1 = f"{str (v)}"
-    if lon1 != " " and lat1 != " ":
-        gps.stop ()
-
-gps.configure (on_location=on_location)
-gps.start()
-st.write(f"{lat1}, {lon1}")
 st.markdown("<h1 class='title'>MAJESTIC AGRO</h1>", unsafe_allow_html=True)
 st.markdown("<h4 class='tit'>'Crop recommendation is a valuable guide for farmers, providing them with expert advice on selecting the most suitable crops to cultivate based on factors like climate, soil conditions, and market demand. This guidance empowers farmers to make informed decisions about crop selection, optimizing yield, profitability, and sustainability. With precise crop recommendations, farmers can maximize resource utilization, minimize environmental impact, and enhance their agricultural practices, ultimately contributing to food security and sustainable farming for a brighter future.'</h2>",unsafe_allow_html=True)
