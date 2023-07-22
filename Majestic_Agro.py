2bdb5da354e65bcab9d23a4fb883ba246ac53ba9
Arulselvan-65 <121343334+Arulselvan-65@users.noreply.github.com>
2023-07-22 09:25:37 +0530

    Update Majestic_Agro.py

--- Majestic_Agro.py
+++ Majestic_Agro.py
@@ -3,6 +3,7 @@ import streamlit as st
 from PIL import Image
 import altair as at
 import csv
+from plyer import gps
 st.set_page_config(layout="wide")
 hide = """
          <style>
@@ -39,5 +40,40 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+try:
+    def on_location (self, **kwargs):
+        for k,v in kwargs.items():
+            if k == "speed":
+                    break
+            if k == "lat":
+                    self.lat1 = f"{str(v)}"
+            if k == "lon":
+                    self.lon1 = f"{str(v)}"
+        if self.lon1 != " " and self.lat1 != " ":
+                gps.stop()
+        api = "2af0b7632c24e579e006a7757b110960"
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={10.827022052142183}&lon={77.06067873809651}&appid={api}"
+        self.x = url.json ()
+        if self.x ["cod"] != "404":
+            temp = round (self.x ["main"] ["temp"] - 273.15)
+            humi = self.x ["main"] ["humidity"]
+            weather = self.x ["weather"] [0] ["main"]
+            feels = round (self.x ["main"] ["feels_like"] - 273.15)
+            wind = round (self.x ["wind"] ["speed"] * 18 / 5)
+            id1 = str (self.x ["weather"] [0] ["id"])
+            if id1 == "800":
+                self.scr1.ids.img_src.source = "sunny"
+            if "200" <= id1 <= "232":
+                self.scr1.ids.img_src.source = "thunder"
+            if "300" <= id1 <= "321" or "500" <= id1 <= "531":
+                self.scr1.ids.img_src.source = "rain"
+            if "600" <= id1 <= "622":
+                self.scr1.ids.img_src.source = "snow"
+            if "701" <= id1 <= "781":
+                self.scr1.ids.img_src.source = "fog"
+            if "801" <= id1 <= "804":
+                self.scr1.ids.img_src.source = "clouds"
+except:
+    pass
 st.markdown("<h1 class='title'>MAJESTIC AGRO</h1>", unsafe_allow_html=True)
 st.markdown("<h4 class='tit'>'Crop recommendation is a valuable guide for farmers, providing them with expert advice on selecting the most suitable crops to cultivate based on factors like climate, soil conditions, and market demand. This guidance empowers farmers to make informed decisions about crop selection, optimizing yield, profitability, and sustainability. With precise crop recommendations, farmers can maximize resource utilization, minimize environmental impact, and enhance their agricultural practices, ultimately contributing to food security and sustainable farming for a brighter future.'</h2>",unsafe_allow_html=True)
